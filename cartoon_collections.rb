cc708f22333a9a7a5139730cacfe4a68bd85002a
deeseert <gio.amms@gmail.com>
2019-02-18 16:40:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(name)
+  new_arr = []
+  name.each_with_index do |item, index|
+    new_arr.push("#{index + 1} #{item}")
+  end
+  puts new_arr
+end
+
+def summon_captain_planet(planeteer)
+  ult_plan = []
+  planeteer.map do |x|
+    ult_plan << x.capitalize + "!"
+  end
+  return ult_plan
+end
+
+def long_planeteer_calls(arr)
+  arr.any? do |w|
+    w.length > 4
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(form)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  form.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
